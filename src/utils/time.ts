08d49ab60c14186de24a78d7d1d33a95c573f250
Stefan Dunn <stefannicholasdunn@googlemail.com>
2023-07-04 20:52:52 +0100

    Add files via upload

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,10 +1,5 @@
 import moment from "moment-timezone";
 
 export const getLondonTime = async (): Promise<number> => {
-  const dateInfo = await fetch(
-    "http://worldtimeapi.org/api/timezone/Europe/London"
-  ).then((response) => response.json());
-  const time = moment(dateInfo.datetime);
-  console.log({ dateInfo });
   return parseInt(moment().tz("Europe/London").format("HHmm"));
 };
